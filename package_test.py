e5ae4dd1a978902fa16477a0edd93391cf4465da
Chenxi Li <ayanamists@gmail.com>
2025-03-27 14:57:55 +0800

    use fatjar

--- package_test.py
+++ package_test.py
@@ -7,5 +7,6 @@ def taie_trans(original, target):
     app_dir = p.join(p.dirname(p.dirname(current_dir)), 'app')
     subprocess.run(f"gradle taieTrans --args='{original} {target} 17'", shell=True, cwd=app_dir)
 
+file = "Java-1.0-SNAPSHOT-jar-with-dependencies"
 current_dir = p.dirname(p.realpath(__file__))
-taie_trans(p.join(current_dir, 'target/Java-1.0-SNAPSHOT.jar'), p.join(current_dir, 'taie-trans/Java-1.0-SNAPSHOT-taie-trans.jar'))
+taie_trans(p.join(current_dir, f'target/{file}.jar'), p.join(current_dir, f'taie-trans/{file}-taie-trans.jar'))
