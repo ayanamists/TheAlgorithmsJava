5fe030045ffe20ce7b9efc6a9bb50a3aa552cdc9
Chenxi Li <ayanamists@gmail.com>
2025-03-27 14:09:22 +0800

    Submodules

--- package_test.py
+++ package_test.py
@@ -5,7 +5,7 @@ import subprocess
 def taie_trans(original, target):
     current_dir = p.dirname(p.realpath(__file__))
     app_dir = p.join(p.dirname(p.dirname(current_dir)), 'app')
-    subprocess.run(f"gradle taieTrans --args='{original} {target}'", shell=True, cwd=app_dir)
+    subprocess.run(f"gradle taieTrans --args='{original} {target} 17'", shell=True, cwd=app_dir)
 
 current_dir = p.dirname(p.realpath(__file__))
 taie_trans(p.join(current_dir, 'target/Java-1.0-SNAPSHOT.jar'), p.join(current_dir, 'taie-trans/Java-1.0-SNAPSHOT-taie-trans.jar'))
